f0fb549d258427379e8a93d52ccada2e4218215f
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-10-05 22:20:11 +0700

    Create display card of agent

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+import { Card } from "./card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [agents, setAgents] = useState([]);
+
+  useEffect(() => {
+    fetch("https://valorant-api.com/v1/agents")
+      .then((r) => r.json())
+      .then((json) => {
+        setAgents([...json.data]);
+      })
+      .catch((e) => console.log(e));
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {agents.length ? <Card agent={agents[0]}></Card> : <p>Loading ... </p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
