7cb58059759a72c1ded55af7f7afe0f70006946d
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-10-06 05:25:55 +0700

    feat: Add hover effect on card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,32 @@
 import "../styles/card.css";
 
+// const card = document.querySelector(".card-container");
+const motionMatchMedia = window.matchMedia("(prefers-reduced-motion)");
+const THRESHOLD = 15;
+
+function handleHover(e) {
+  const { clientX, clientY, currentTarget } = e;
+  const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+
+  const horizontal = (clientX - offsetLeft) / clientWidth;
+  const vertical = (clientY - offsetTop) / clientHeight;
+  const rotateX = -(THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+  const rotateY = -(vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+
+  this.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+}
+
+function resetStyles(e) {
+  this.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+}
+
+document.querySelectorAll(".card-container").forEach((card) => {
+  if (!motionMatchMedia.matches) {
+    card.addEventListener("mousemove", handleHover);
+    card.addEventListener("mouseleave", resetStyles);
+  }
+});
+
 function FaceCard({ agent }) {
   // console.log(agent);
   const agentBg = agent.background;
